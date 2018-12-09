5f6b90ed0525ea8f1323b601c47ea79154f030a9
KondiGitHub <haichary@yahoo.co.in>
2018-12-09 13:36:34 -0500

    Update ANLPage.java

--- src/main/java/com/TestingFrameWork/Pages/ANLPage.java
+++ src/main/java/com/TestingFrameWork/Pages/ANLPage.java
@@ -1,1 +1,31 @@
+package com.TestingFrameWork.Pages;
+
+import org.openqa.selenium.By;
+
+public class ANLPage extends Pages {
+	
+	public  boolean isSSNTextBoxfound() {
+		 if (getDriver().findElement(By.id("appSSN"))!=null) {
+			 return true;
+		 }
+		 return false;
+	}
+	
+	public   boolean iZipCodeTextBoxfound() {
+		 if (getDriver().findElement(By.id("applyVTO_applicantVO_zip_id"))!=null) {
+			 return true;
+		 }
+		 return false;
+	}
+	
+	public void enterSSNAndZipcode(Integer ssn, Integer zipCode) {
+		getDriver().findElement(By.id("appSSN")).sendKeys(ssn.toString());
+		getDriver().findElement(By.id("applyVTO_applicantVO_zip_id")).sendKeys(zipCode.toString());
+	}
+	
+	public void clickOnContinueButton(String buttonText) {
+		getDriver().findElement(By.id("termSubmitButton")).click();
+	}
+
+}
 
