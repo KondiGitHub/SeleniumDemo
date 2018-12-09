86ee1914da3e5e84b0ec4f337ababa1c9fec6d9f
KondiGitHub <haichary@yahoo.co.in>
2018-12-09 13:34:49 -0500

    Update Browser.java

--- src/main/java/com/TestingFrameWork/Browsers/Browser.java
+++ src/main/java/com/TestingFrameWork/Browsers/Browser.java
@@ -1,1 +1,31 @@
 
+package com.TestingFrameWork.Browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browser {
+
+	static WebDriver driver = new FirefoxDriver();
+	
+	public static void goTo(String url) {
+		driver.get(url);
+		
+	}
+
+	public static Object title() {
+		// TODO Auto-generated method stub
+		return driver.getTitle();
+	}
+
+	
+	public static void close() {
+		driver.close();
+		
+	}
+	
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+}
